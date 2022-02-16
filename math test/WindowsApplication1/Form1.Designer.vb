7b133769d7cc79a29161b28430331ebe50c6db30
ahmadfathy97 <af1381@fayoum.edu.eg>
2022-02-16 02:35:53 +0200

    first app

---
+++ math test/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.q1_num1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.q1_num2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.q2_num2 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.q2_num1 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.q3_num2 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.q3_num1 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.q4_num2 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.q4_num1 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.q5_num2 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.q5_num1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(109, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(278, 34)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "اختبار عمليات الجمع"
+        '
+        'q1_num1
+        '
+        Me.q1_num1.AutoSize = True
+        Me.q1_num1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q1_num1.Location = New System.Drawing.Point(107, 176)
+        Me.q1_num1.Name = "q1_num1"
+        Me.q1_num1.Size = New System.Drawing.Size(0, 17)
+        Me.q1_num1.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(141, 176)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(18, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "+"
+        '
+        'q1_num2
+        '
+        Me.q1_num2.AutoSize = True
+        Me.q1_num2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q1_num2.Location = New System.Drawing.Point(180, 176)
+        Me.q1_num2.Name = "q1_num2"
+        Me.q1_num2.Size = New System.Drawing.Size(0, 17)
+        Me.q1_num2.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(245, 176)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(18, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "="
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button1.ForeColor = System.Drawing.Color.Green
+        Me.Button1.Location = New System.Drawing.Point(393, 454)
+        Me.Button1.Name = "Button1"
+        Me.Button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Button1.Size = New System.Drawing.Size(92, 29)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "تسليم"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 460)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 17)
+        Me.Label6.TabIndex = 8
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(245, 212)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(18, 17)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "="
+        '
+        'q2_num2
+        '
+        Me.q2_num2.AutoSize = True
+        Me.q2_num2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q2_num2.Location = New System.Drawing.Point(180, 212)
+        Me.q2_num2.Name = "q2_num2"
+        Me.q2_num2.Size = New System.Drawing.Size(0, 17)
+        Me.q2_num2.TabIndex = 11
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(141, 212)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(18, 17)
+        Me.Label9.TabIndex = 10
+        Me.Label9.Text = "+"
+        '
+        'q2_num1
+        '
+        Me.q2_num1.AutoSize = True
+        Me.q2_num1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q2_num1.Location = New System.Drawing.Point(107, 208)
+        Me.q2_num1.Name = "q2_num1"
+        Me.q2_num1.Size = New System.Drawing.Size(0, 17)
+        Me.q2_num1.TabIndex = 9
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(245, 245)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(18, 17)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "="
+        '
+        'q3_num2
+        '
+        Me.q3_num2.AutoSize = True
+        Me.q3_num2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q3_num2.Location = New System.Drawing.Point(180, 245)
+        Me.q3_num2.Name = "q3_num2"
+        Me.q3_num2.Size = New System.Drawing.Size(0, 17)
+        Me.q3_num2.TabIndex = 16
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(141, 245)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(18, 17)
+        Me.Label13.TabIndex = 15
+        Me.Label13.Text = "+"
+        '
+        'q3_num1
+        '
+        Me.q3_num1.AutoSize = True
+        Me.q3_num1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q3_num1.Location = New System.Drawing.Point(107, 245)
+        Me.q3_num1.Name = "q3_num1"
+        Me.q3_num1.Size = New System.Drawing.Size(0, 17)
+        Me.q3_num1.TabIndex = 14
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(245, 276)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(18, 17)
+        Me.Label15.TabIndex = 22
+        Me.Label15.Text = "="
+        '
+        'q4_num2
+        '
+        Me.q4_num2.AutoSize = True
+        Me.q4_num2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q4_num2.Location = New System.Drawing.Point(180, 276)
+        Me.q4_num2.Name = "q4_num2"
+        Me.q4_num2.Size = New System.Drawing.Size(0, 17)
+        Me.q4_num2.TabIndex = 21
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(141, 276)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(18, 17)
+        Me.Label17.TabIndex = 20
+        Me.Label17.Text = "+"
+        '
+        'q4_num1
+        '
+        Me.q4_num1.AutoSize = True
+        Me.q4_num1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q4_num1.Location = New System.Drawing.Point(107, 276)
+        Me.q4_num1.Name = "q4_num1"
+        Me.q4_num1.Size = New System.Drawing.Size(0, 17)
+        Me.q4_num1.TabIndex = 19
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(245, 308)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(18, 17)
+        Me.Label19.TabIndex = 27
+        Me.Label19.Text = "="
+        '
+        'q5_num2
+        '
+        Me.q5_num2.AutoSize = True
+        Me.q5_num2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q5_num2.Location = New System.Drawing.Point(180, 308)
+        Me.q5_num2.Name = "q5_num2"
+        Me.q5_num2.Size = New System.Drawing.Size(0, 17)
+        Me.q5_num2.TabIndex = 26
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(141, 308)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(18, 17)
+        Me.Label21.TabIndex = 25
+        Me.Label21.Text = "+"
+        '
+        'q5_num1
+        '
+        Me.q5_num1.AutoSize = True
+        Me.q5_num1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.q5_num1.Location = New System.Drawing.Point(107, 304)
+        Me.q5_num1.Name = "q5_num1"
+        Me.q5_num1.Size = New System.Drawing.Size(0, 17)
+        Me.q5_num1.TabIndex = 24
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(280, 169)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {-1981284353, -1966660860, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(107, 24)
+        Me.NumericUpDown1.TabIndex = 29
+        Me.NumericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(280, 204)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {-1981284353, -1966660860, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(107, 24)
+        Me.NumericUpDown2.TabIndex = 30
+        Me.NumericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(280, 238)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {-1981284353, -1966660860, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(107, 24)
+        Me.NumericUpDown3.TabIndex = 32
+        Me.NumericUpDown3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(280, 270)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {-1981284353, -1966660860, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(107, 24)
+        Me.NumericUpDown4.TabIndex = 31
+        Me.NumericUpDown4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'NumericUpDown5
+        '
+        Me.NumericUpDown5.Location = New System.Drawing.Point(280, 304)
+        Me.NumericUpDown5.Maximum = New Decimal(New Integer() {-1981284353, -1966660860, 0, 0})
+        Me.NumericUpDown5.Name = "NumericUpDown5"
+        Me.NumericUpDown5.Size = New System.Drawing.Size(107, 24)
+        Me.NumericUpDown5.TabIndex = 33
+        Me.NumericUpDown5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 495)
+        Me.Controls.Add(Me.NumericUpDown5)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.q5_num2)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.q5_num1)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.q4_num2)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.q4_num1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.q3_num2)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.q3_num1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.q2_num2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.q2_num1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.q1_num2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.q1_num1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "math test"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents q1_num1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents q1_num2 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents q2_num2 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents q2_num1 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents q3_num2 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents q3_num1 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents q4_num2 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents q4_num1 As System.Windows.Forms.Label
+    Friend WithEvents Label19 As System.Windows.Forms.Label
+    Friend WithEvents q5_num2 As System.Windows.Forms.Label
+    Friend WithEvents Label21 As System.Windows.Forms.Label
+    Friend WithEvents q5_num1 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown5 As System.Windows.Forms.NumericUpDown
+End Class
---
+++ math test/WindowsApplication1/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Dim degree As Single = 0
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles q1_num1.Click
+
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim r As New Random
+        Dim q1_n1 As Single = r.Next(0, 100)
+        Dim q1_n2 As Single = r.Next(0, 100)
+        Dim q2_n1 As Single = r.Next(0, 100)
+        Dim q2_n2 As Single = r.Next(0, 100)
+        Dim q3_n1 As Single = r.Next(0, 100)
+        Dim q3_n2 As Single = r.Next(0, 100)
+        Dim q4_n1 As Single = r.Next(0, 100)
+        Dim q4_n2 As Single = r.Next(0, 100)
+        Dim q5_n1 As Single = r.Next(0, 100)
+        Dim q5_n2 As Single = r.Next(0, 100)
+        q1_num1.Text = q1_n1
+        q1_num2.Text = q1_n2
+        q2_num1.Text = q2_n1
+        q2_num2.Text = q2_n2
+        q3_num1.Text = q3_n1
+        q3_num2.Text = q3_n2
+        q4_num1.Text = q4_n1
+        q4_num2.Text = q4_n2
+        q5_num1.Text = q5_n1
+        q5_num2.Text = q5_n2
+    End Sub
+
+    Private Sub Button1_Click_2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'If (NumericUpDown1.Text.Length = 0 Or NumericUpDown2.Text.Length = 0 Or NumericUpDown3.Text.Length = 0 Or NumericUpDown4.Text.Length = 0 Or NumericUpDown5.Text.Length = 0) Then
+        'MsgBox("You must solve all problems")
+        'Else
+        If (Int(q1_num1.Text) + Int(q1_num2.Text) = NumericUpDown1.Value) Then
+            degree = degree + 1
+        End If
+        If (Int(q2_num1.Text) + Int(q2_num2.Text) = NumericUpDown2.Value) Then
+            degree = degree + 1
+        End If
+        If (Int(q3_num1.Text) + Int(q3_num2.Text) = NumericUpDown3.Value) Then
+            degree = degree + 1
+        End If
+        If (Int(q4_num1.Text) + Int(q4_num2.Text) = NumericUpDown4.Value) Then
+            degree = degree + 1
+        End If
+        If (Int(q5_num1.Text) + Int(q5_num2.Text) = NumericUpDown5.Value) Then
+            degree = degree + 1
+        End If
+        If (degree >= 3) Then
+            Label6.ForeColor = Color.Green
+        Else
+            Label6.ForeColor = Color.Red
+        End If
+        Label6.Text = degree & " / 5"
+        degree = 0
+        'End If
+    End Sub
+
+    Private Sub MessageBox(ByVal p1 As Object)
+        Throw New NotImplementedException
+    End Sub
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown1.ValueChanged
+
+    End Sub
+End Class
