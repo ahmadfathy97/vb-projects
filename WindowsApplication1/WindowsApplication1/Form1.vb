7bc4845e4f465bc0cd69d30cb56bbdd29d618d88
ahmadfathy97 <af1381@fayoum.edu.eg>
2022-02-28 21:38:22 +0200

    paint

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            MsgBox(CheckBox1.Text)
+        End If
+    End Sub
+End Class
