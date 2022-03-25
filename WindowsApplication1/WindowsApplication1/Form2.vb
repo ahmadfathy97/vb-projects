9245a7f879a060c3c32d3e3fe071bc28c60b19a2
ahmadfathy97 <af1381@fayoum.edu.eg>
2022-03-25 17:08:30 +0200

    remove the empty projects

--- WindowsApplication1/WindowsApplication1/Form2.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form2
-
-    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Label1.Text = Module1.st
-    End Sub
-End Class
--- WindowsApplication1/WindowsApplication1/Module1.vb
+++
@@ -1,3 +0,0 @@
-﻿Module Module1
-    Public st As String = "test"
-End Module
